65b966ccfad5b6f68ee93dc5ca60737f662eaa4a
Kelvin Thung <domainkelvin99@gmail.com>
2024-07-01 17:26:00 +0800

    firstPush

---
+++ App.tsx
@@ -0,0 +1,79 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import * as LocalAuthentication from 'expo-local-authentication';
+
+const handleBiometricAuth = async () => {
+  const biometricAuth = await LocalAuthentication.authenticateAsync({
+    promptMessage: 'login with Biometric',
+}).then(Res => {
+    if (Res.success == true) {
+        console.log("ok")
+    }
+    else {
+        console.log("not ok")
+    }
+});
+}
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <View > 
+        <Text>1</Text>
+        <Button onPress={() => { handleBiometricAuth() }} title="FingerPrint test"/>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
